1e83a91940df8dbf43226933c06590dd0537778a
Martynas Kasparavicius <martis.kasparavicius@gmail.com>
2021-01-18 14:46:22 +0200

    fonts added

--- src/App.vue
+++ src/App.vue
@@ -12,10 +12,14 @@
 
     body {
         margin: 0;
+
+        font-weight: 400;
+        font-size: 16px;
+        letter-spacing: 0.5px;
     }
 
     #app {
-        font-family: Avenir, Helvetica, Arial, sans-serif;
+        font-family: 'Open Sans', 'Roboto';
         -webkit-font-smoothing: antialiased;
         -moz-osx-font-smoothing: grayscale;
         text-align: center;
@@ -43,5 +47,75 @@
         font-style: normal;
         font-weight: 400;
         src: url("/fonts/MaterialIcons/MaterialIcons-Regular.woff2") format('woff2');
+    }
+
+    @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 300;
+        src: url("/fonts/OpenSans-Light.ttf") format('ttf');
+    }
+
+    @font-face {
+        font-family: "Open Sans";
+        font-style: italic;
+        font-weight: 300;
+        src: url("/fonts/OpenSans-LightItalic.ttf") format('ttf');
+    }
+
+    @font-face {
+        font-family: 'Open Sans';
+        font-style: normal;
+        font-weight: 400;
+        src: url("/fonts/OpenSans-Regular.ttf") format('ttf');
+    }
+
+    @font-face {
+        font-family: "Open Sans";
+        font-style: italic;
+        font-weight: 400;
+        src: url("/fonts/OpenSans-Italic.ttf") format('ttf');
+    }
+
+    @font-face {
+        font-family: "Open Sans";
+        font-style: normal;
+        font-weight: 600;
+        src: url("/fonts/OpenSans-SemiBold.ttf") format('ttf');
+    }
+
+    @font-face {
+        font-family: "Open Sans";
+        font-style: italic;
+        font-weight: 600;
+        src: url("/fonts/OpenSans-SemiBoldItalic.ttf") format('ttf');
+    }
+
+    @font-face {
+        font-family: "Open Sans";
+        font-style: normal;
+        font-weight: 700;
+        src: url("/fonts/OpenSans-Bold.ttf") format('ttf');
+    }
+
+    @font-face {
+        font-family: "Open Sans";
+        font-style: italic;
+        font-weight: 700;
+        src: url("/fonts/OpenSans-BoldItalic.ttf") format('ttf');
+    }
+
+    @font-face {
+        font-family: "Open Sans";
+        font-style: normal;
+        font-weight: 800;
+        src: url("/fonts/OpenSans-ExtraBold.ttf") format('ttf');
+    }
+
+    @font-face {
+        font-family: "Open Sans";
+        font-style: italic;
+        font-weight: 800;
+        src: url("/fonts/OpenSans-ExtraBoldItalic.ttf") format('ttf');
     }
 </style>
