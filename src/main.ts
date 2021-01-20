9ab2a56af801a8d82ae91cba696d16e0f35daf40
MartynasK <33058459+MartynasKasp@users.noreply.github.com>
2021-01-20 22:46:31 +0200

    Revert "Feature/seo"

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,4 @@
 import Vue from 'vue';
-import VueMeta from 'vue-meta';
 import {
     MdButton, MdField, MdMenu, MdList,
     MdSnackbar, MdIcon, MdDialog, MdApp,
@@ -15,7 +14,6 @@ import store from './store';
 
 Vue.config.productionTip = false;
 
-Vue.use(VueMeta);
 Vue.use(MdButton);
 Vue.use(MdField);
 Vue.use(MdMenu);
@@ -34,16 +32,4 @@ new Vue({
     router,
     store,
     render: (h) => h(App),
-    metaInfo: () => ({
-        title: 'Home',
-        titleTemplate: '%s | iKNOW Points Calculator',
-        htmlAttrs: {
-            lang: 'en',
-        },
-        meta: [
-            { name: 'description', content: 'iKNOW board game playing made easier and faster!' },
-            { name: 'author', content: 'MartynasKasp' },
-            { name: 'keywords', content: 'iknow, iknow board game, iknow calculator, iknow board game calculator, iknow points calculator, iknow points, iknow online, iknow points online, iknow game, iknow board, martynaskasp, tactic, tactic iknow' },
-        ],
-    }),
 }).$mount('#app');
--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,3 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-const path = require('path');
-const PrerenderSPAPlugin = require('prerender-spa-plugin');
-
 module.exports = {
     css: {
         loaderOptions: {
@@ -13,14 +9,5 @@ module.exports = {
     devServer: {
         port: '3000',
         https: false,
-    },
-    publicPath: process.env.NODE_ENV === 'production' ? '/iknow/' : '/',
-    configureWebpack: {
-        plugins: [
-            new PrerenderSPAPlugin({
-                staticDir: path.join(__dirname, 'dist'),
-                routes: ['/'],
-            }),
-        ],
     },
 };
