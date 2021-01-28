f70d004dfb8d87418732a7e1c5533d0df07ef2e6
Martynas Kasparavicius <martis.kasparavicius@gmail.com>
2021-01-28 14:11:28 +0200

    store expiration added

--- src/store/store.ts
+++ src/store/store.ts
@@ -14,30 +14,35 @@ export default class Store<S> extends Vuex.Store<S> {
             modulePath = path;
         }
 
-        // eslint-disable-next-line @typescript-eslint/no-unused-vars
-        const localState = LocalStorageManager.getResolvedStateData() || {};
+        if (!LocalStorageManager.isStateExpired()) {
+            // eslint-disable-next-line @typescript-eslint/no-unused-vars
+            const localState = LocalStorageManager.getResolvedStateData() || {};
 
-        let payload: any = null;
-        const moduleData = localState[modulePath];
+            let payload: any = null;
+            const moduleData = localState[modulePath];
 
-        // If there is module data in local storage
-        // we set it as a payload, which overrides module's
-        // initial state to that saved data later on
-        if (moduleData) {
-            payload = moduleData;
-            this.saveDataToStorage(modulePath, moduleData, localState);
+            // If there is module data in local storage
+            // we set it as a payload, which overrides module's
+            // initial state to that saved data later on
+            if (moduleData) {
+                payload = moduleData;
+                this.saveDataToStorage(modulePath, moduleData, localState);
+            } else {
+                const moduleState = module.state ? module.state : {};
+                this.saveDataToStorage(modulePath, moduleState, localState);
+            }
+
+            // Overriding initial module state with the one saved in local storage
+            if (payload) {
+                // eslint-disable-next-line no-param-reassign
+                module.state = payload;
+            }
+
+            super.registerModule(modulePath, module, options);
         } else {
-            const moduleState = module.state ? module.state : {};
-            this.saveDataToStorage(modulePath, moduleState, localState);
+            this.saveDataToStorage(modulePath, module.state, {});
+            super.registerModule(modulePath, module, options);
         }
-
-        // Overriding initial module state with the one saved in local storage
-        if (payload) {
-            // eslint-disable-next-line no-param-reassign
-            module.state = payload;
-        }
-
-        super.registerModule(modulePath, module, options);
     }
 
     saveDataToStorage(path: string, state: any, storeState: any) {
